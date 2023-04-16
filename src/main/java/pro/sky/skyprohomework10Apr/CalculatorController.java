f2c0e778f5a5bdacb65b34186295fa0ec85e89e9
Valery Maltsev <lisowsky@yandex.ru>
2023-04-16 18:35:26 +0200

    Created New Branch

---
+++ src/main/java/pro/sky/skyprohomework10Apr/CalculatorController.java
@@ -0,0 +1,9 @@
+package pro.sky.skyprohomework10Apr;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+}
